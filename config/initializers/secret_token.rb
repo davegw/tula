fa5be82a2eee6106d96509e31ab848e8990b9897
davidgertmenianwong <david@giftrocket.com>
2013-07-21 00:29:41 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tula::Application.config.secret_token = 'd193c1d924bae56776ffe28ddc271f074b1a129d676d65a5c95c82a64ff1d97d97aa808dbe1b7e104943c0c1213b1c442448c2f8f7cd97107594c7eddf953e79'
