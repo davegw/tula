35a8602b081bbe9d4ecce2f478ea27fd0a2efad8
davidgertmenianwong <david@giftrocket.com>
2013-09-25 21:16:01 -0700

    edit formating on cms-admin panel

--- app/assets/javascripts/comfortable_mexican_sofa/admin/application.js
+++ app/assets/javascripts/comfortable_mexican_sofa/admin/application.js
@@ -1,1 +1,5 @@
 // Overwrite this file in your application /app/assets/javascripts/comfortable_mexican_sofa/admin/application.js 
+
+$(function() {
+  $('#edit_post .form_element:nth(4)').hide();  
+});
