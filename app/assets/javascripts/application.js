64d7d910772435cd8bd924b241c9b6d8568fb8f2
davidgertmenianwong <david@giftrocket.com>
2013-09-27 00:53:01 -0700

    precompile again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,8 +39,8 @@ $(function() {
 // Add chart
 $(function() {
   var canvas = document.getElementsByTagName('canvas')[0];
-  canvas.width  = 900px;
-  canvas.height = 450px;
+  canvas.width  = 900;
+  canvas.height = 450;
   var lineChartData = {
     labels : ["1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", 
       "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013"],
