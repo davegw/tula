cb42005cefe9fa60e396b5d9fa19a9506b63e060
davidgertmenianwong <david@giftrocket.com>
2013-10-02 00:10:36 -0700

    SOFT LAUNCH! - remove coming soon page

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ Tula::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#coming_soon'
+  root :to => 'home#index'
 
   match '/coming-soon' => 'home#coming_soon', :as => :coming_soon
   match '/home' => 'home#index', :as => :home
