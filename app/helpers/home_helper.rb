4fb5b22788e09f95ae27d0cf5c09b4f2d3951b32
davidgertmenianwong <david@giftrocket.com>
2013-10-01 22:28:46 -0700

    create stock examples styling and content; revise general prospectus layout

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -17,8 +17,28 @@ module HomeHelper
     end
   end
   def render_prospectus_sub_section(sub_section, hash)
-    haml_tag :a, :id => hash
-    haml_tag :div, sub_section, :class => 'prospectus_sub_heading'
-    yield
+    haml_tag :div, :class => 'prospectus_sub_section' do
+      haml_tag :a, :id => hash
+      haml_tag :div, sub_section, :class => 'prospectus_sub_heading'
+      yield
+    end
+  end
+
+  def render_example_stock_section
+    haml_tag :div, :class => 'example_stocks_section' do
+      haml_tag :p, 'Notable Example Stocks:', :class => 'example_stocks_header'
+      haml_tag :ul do
+        yield
+      end
+    end
+  end
+  def render_example_stock(stock, industry)
+    haml_tag :li do
+      haml_tag :p, :class => 'example_stock' do
+        haml_tag :strong, stock
+        haml_tag :span, industry + ' - '
+        yield
+      end
+    end
   end
 end
