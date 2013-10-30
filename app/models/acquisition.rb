a9e15e270bff3fe663d7bf6cdacc3e35bef4d4ac
davidgertmenianwong <david@giftrocket.com>
2013-10-29 20:06:30 -0700

    add acquisition convenience method for caluclating return

--- app/models/acquisition.rb
+++ app/models/acquisition.rb
@@ -1,3 +1,8 @@
 class Acquisition < ActiveRecord::Base
   attr_accessible :acquisition_date, :acquisition_price, :initial_date, :initial_price, :return, :year, :company
+
+  def calc_return!
+    self.return = (acquisition_price - initial_price)/initial_price
+    self.save
+  end
 end
