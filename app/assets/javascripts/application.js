f0ae003863cabee36cad94c8f38c4bad7119d0b5
davidgertmenianwong <david@giftrocket.com>
2013-11-29 15:48:29 -0800

    update chart dataset for 11/15/13 numbers and adjust y axis

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -93,7 +93,7 @@ $(function() {
           pointColor : "rgba(255,205,56,1)",
           pointStrokeColor : "#fff",
           data : [10000, 14000, 23520, 25402, 32768, 50135, 41612, 41196, 30897, 40784, 49349, 
-            53790, 59707, 69260, 50560, 65728, 80188, 68962, 79306, 103098],
+            53790, 59707, 69260, 50560, 65728, 80188, 68962, 79306, 110235],
           title : "TULA"
         },
         {
@@ -102,7 +102,7 @@ $(function() {
           pointColor : "rgba(56,156,255,1)",
           pointStrokeColor : "#fff",
           data : [10000, 13700, 16851, 22412, 28911, 34983, 31834, 28014, 21851, 28188, 31288, 
-            32853, 38109, 40015, 25209, 31764, 35575, 36287, 42093, 49669],
+            32853, 38109, 40015, 25209, 31764, 35575, 36287, 42093, 52616],
           title : "S&P 500"
         }
       ],
@@ -110,7 +110,13 @@ $(function() {
     var options = {
       datasetFill : true,
       showLegend: true,
-      bezierCurve : true
+      bezierCurve : true,
+      // Scale gets whacky when over 110,000 for some reason.
+      // Use scaleOverride to manually set scale on the y-axis.
+      scaleOverride: true,
+      scaleSteps: 11,
+      scaleStepWidth: 10000,
+      scaleStartValue: 10000
     };
     new Chart(document.getElementById("myChart").getContext("2d")).Line(lineChartData, options);
   };
