b1ff9e1f44774abb0eecc92c1216cd6ce17d5637
davidgertmenianwong <dgertmenianwong@gmail.com>
2015-02-09 15:11:57 -0800

    Refactor acquisitions data to controller and DRY view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+
+  # Constants
+  INITIAL_INVESTMENT = 100000
+  CURRENT_YEAR = 2015
+
   def index
   end
 
@@ -11,11 +16,23 @@ class HomeController < ApplicationController
   end
 
   def why
-    @initial_investment = 100000
-    @historic_returns = HistoricReturn.where("year >= ?", 1994).order(:year)
+    @initial_investment = INITIAL_INVESTMENT
+    @historic_returns = HistoricReturn.where('year >= ?', 1994).order(:year)
   end
 
   def acquisitions
+    @years = (2013..CURRENT_YEAR).to_a
+    @acquisitions = @years.reduce({}) do |acquisitions, year|
+      acquisitions[year] = Acquisition.where(:year => year).order(:id)
+      acquisitions
+    end
+    @disclosures = {
+      2013 => '* On 6/10/13, the deal for the buy out of Virgin Media by Liberty Global 
+               was completed. Virgin Media shareholders received $17.50 for each share they 
+               owned plus stock in Liberty Global. Total return including both cash payments 
+               and receipt of Liberty Global stock equates to a 35% return over the 1/1/13 
+               valuation.'
+    }
   end
 
   def references
