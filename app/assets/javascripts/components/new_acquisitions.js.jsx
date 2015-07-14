2967494e371251dde1c8a0df7067efda809eeaef
David Gertmenian-Wong <dgertmenianwong@gmail.com>
2015-07-14 16:59:41 -0700

    Finish static data-less implementation of new acquisition form

--- app/assets/javascripts/components/new_acquisitions.js.jsx
+++ app/assets/javascripts/components/new_acquisitions.js.jsx
@@ -19,8 +19,8 @@ var YearSelector = React.createClass({
     });
     return (
       <div className='form-group'>
-        <label>Year</label>
-        <select type='select' ref='year'>
+        <label htmlFor='year'>Year</label>
+        <select name='year' type='select' className='form-control' onChange={this.props.changeHandler} value={this.props.defaultYear}>
           {selectorOptions}
         </select>
       </div>
@@ -28,36 +28,53 @@ var YearSelector = React.createClass({
   }
 });
 
-var TextFormField = React.createClass({
-  render: function() {
-    return (
-      <div className='form-group'>
-        <label for={this.props.inputValue}>{this.props.label}</label>
-        <input name={this.props.inputValue} type='text' className='form-control' />
-      </div>
-    );
-  }
-});
-
 var NewAcquisitionForm = React.createClass({
+  getInitialState: function() {
+    return {
+      year: '2'
+    };
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var newAcquistion = {
+      year: this.state.year,
+      company: React.findDOMNode(this.refs.company).value.trim(),
+      initial_price: React.findDOMNode(this.refs.initial_price).value.trim(),
+      acquisition_price: React.findDOMNode(this.refs.acquisition_price).value.trim(),
+    };
+    console.log(newAcquistion);
+  },
+
+  yearSelection: function(e) {
+    this.setState({
+      year: e.target.value
+    });
+  },
+
   render: function() {
     var textFormFields = [
-        {label: 'Company', inputValue: 'company'},
-        {label: 'Price at beginning of Year', inputValue: 'initial_price'},
-        {label: 'Buy Out Price', inputValue: 'acquisition_price'},
-      ].map(function(field, idx) {
-        return (
-          <TextFormField
-            key={idx}
-            label={field.label}
-            inputValue={field.inputValue}
+      {label: 'Company', inputValue: 'company'},
+      {label: 'Price at Beginning of Year', inputValue: 'initial_price'},
+      {label: 'Buy Out Price', inputValue: 'acquisition_price'},
+    ].map(function(field, idx) {
+      return (
+        <div key={idx} className='form-group'>
+          <label htmlFor={field.inputValue}>{field.label}</label>
+          <input
+            name={field.inputValue}
+            type='text'
+            className='form-control'
+            ref={field.inputValue}
           />
-        );
-      });
+        </div>
+      );
+    });
     return (
-      <form>
-        <YearSelector />
+      <form onSubmit={this.handleSubmit}>
+        <YearSelector defaultYear={this.state.year} changeHandler={this.yearSelection} />
         {textFormFields}
+        <input type='submit' className='btn btn-success' value='Add New Acquisition' />
       </form>
     );
   }
