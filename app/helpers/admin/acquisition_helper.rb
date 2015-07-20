b147c6b6b777a9dd61d6dfe8cde31ef053557773
David Gertmenian-Wong <dgertmenianwong@gmail.com>
2015-07-20 10:39:00 -0700

    Add method for formatting to local times

--- app/helpers/admin/acquisition_helper.rb
+++ app/helpers/admin/acquisition_helper.rb
@@ -2,4 +2,10 @@ module Admin::AcquisitionHelper
   def return_calculator(initial, final)
     (final.to_f - initial.to_f) / initial.to_f
   end
+
+  def local_time_format(time_with_zone)
+    parsed_time = Time.parse(time_with_zone.to_s)
+    local_time = parsed_time.in_time_zone("Pacific Time (US & Canada)")
+    local_time.strftime('%b %d, %Y: %I:%M%p')
+  end
 end
