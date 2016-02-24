688b7bebd3c0766a73644d431206e2cd8f667458
David Gertmenian-Wong <dgertmenianwong@gmail.com>
2016-02-24 13:28:27 -0800

    Update acquisitions controller to automatically add transactions for new year

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
 
   # Constants
   INITIAL_INVESTMENT = 100000
-  CURRENT_YEAR = 2015
 
   def index
   end
@@ -21,16 +20,19 @@ class HomeController < ApplicationController
   end
 
   def acquisitions
-    @years = (2013..CURRENT_YEAR).to_a
-    @acquisitions = @years.reduce({}) do |acquisitions, year|
-      acquisitions[year] = Acquisition.where(:year => year).order(:id)
-      acquisitions
+    @acquisitions = Acquisition.all.sort_by(&:id).reduce({}) do |acq_hash, acquisition|
+      year = acquisition.year
+      acq_hash[year] ||= []
+      acq_hash[year].push(acquisition)
+      acq_hash
     end
+    @years = @acquisitions.keys.sort.reverse
+
     @disclosures = {
-      2013 => '* On 6/10/13, the deal for the buy out of Virgin Media by Liberty Global 
-               was completed. Virgin Media shareholders received $17.50 for each share they 
-               owned plus stock in Liberty Global. Total return including both cash payments 
-               and receipt of Liberty Global stock equates to a 35% return over the 1/1/13 
+      2013 => '* On 6/10/13, the deal for the buy out of Virgin Media by Liberty Global
+               was completed. Virgin Media shareholders received $17.50 for each share they
+               owned plus stock in Liberty Global. Total return including both cash payments
+               and receipt of Liberty Global stock equates to a 35% return over the 1/1/13
                valuation.'
     }
   end
